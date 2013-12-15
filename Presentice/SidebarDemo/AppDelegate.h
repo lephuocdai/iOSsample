dcaa6e1c4c7ad6910364232c31f9abe7044525df
PhuongNQ <nquangphuong@gmail.com>
2013-12-15 17:13:27 +0900

    add frameworks

--- Presentice/SidebarDemo/AppDelegate.h
+++ Presentice/SidebarDemo/AppDelegate.h
@@ -7,8 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, PFLogInViewControllerDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
