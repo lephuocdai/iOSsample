857916c63b004c348000cfb6f909c98fb406d259
lephuocdai <lephuocdaijapan@gmail.com>
2014-09-08 18:03:17 +0900

    Working with dispatch source

--- GooglyPuff_Start_1/GooglyPuff/PhotoCollectionViewController.m
+++ GooglyPuff_Start_1/GooglyPuff/PhotoCollectionViewController.m
@@ -32,6 +32,26 @@ UIActionSheetDelegate>
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+#if DEBUG
+    dispatch_queue_t queue = dispatch_get_main_queue();
+    
+    static dispatch_source_t source = nil;
+    
+    __typeof(self) __weak weakSelf = self;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        source = dispatch_source_create(DISPATCH_SOURCE_TYPE_SIGNAL, SIGSTOP, 0, queue);
+        if (source) {
+            dispatch_source_set_event_handler(source, ^{
+                NSLog(@"weakSelf = %@", weakSelf);
+            });
+            dispatch_resume(source);
+        }
+    });
+#endif
+    
     self.library = [[ALAssetsLibrary alloc] init];
     
     // Background image setup
