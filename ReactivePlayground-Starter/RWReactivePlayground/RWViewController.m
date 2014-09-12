f7255acd2183f963f25ecc36483b4c8d6342d1c1
lephuocdai <lephuocdaijapan@gmail.com>
2014-09-12 17:52:01 +0900

    Adding side effects

--- ReactivePlayground-Starter/RWReactivePlayground/RWViewController.m
+++ ReactivePlayground-Starter/RWReactivePlayground/RWViewController.m
@@ -60,11 +60,16 @@
         self.signInButton.enabled = [signupActive boolValue];
     }];
     
-    [[[self.signInButton rac_signalForControlEvents:UIControlEventTouchUpInside]
+    [[[[self.signInButton rac_signalForControlEvents:UIControlEventTouchUpInside]
+       doNext:^(id x) {
+           self.signInButton.enabled = NO;
+           self.signInFailureText.hidden = YES;
+       }]
       flattenMap:^id(id x) {
           return [self signInSignal];
       }]
       subscribeNext:^(NSNumber *signedIn) {
+          self.signInButton.enabled = YES;
           BOOL success = [signedIn boolValue];
           self.signInFailureText.hidden = success;
           if (success) {
